32f54f068e3ecfce4edf65017cc795a5cbc96bde
Devesh Dayal <devesh95@gmail.com>
2015-05-26 19:51:49 +0530

    finding posts based on input name

--- content_scripts/main.js
+++ content_scripts/main.js
@@ -2,6 +2,25 @@
 
 // TODO: find the HTML container for posts in the group by ID
 
+
+var findPosts = function (name) {
+	var wrapper_element_list =  $('.userContentWrapper');
+	var posts = [];
+
+	wrapper_element_list.each(function (index) {
+		var wrapperElem = $(this);
+		var span_element_list = wrapperElem.find('span');
+
+		span_element_list.each.each(function (index) {
+			var spanElement = $(this);
+			if (spanElement.text().indexOf(name) > -1) {
+				posts.push(wrapperElem);
+			}
+		});
+	});
+};
+
+
 // TODO: take in input through a floating dialog box
 
 // TODO: filter by name, hopefully there's a common class 
