85aa6f475b33c32d0d8641d61ed766b7d60c8303
Devesh Dayal <devesh95@gmail.com>
2015-05-26 18:49:08 +0530

    basic setup and plan

---
+++ content_scripts/main.js
@@ -0,0 +1,9 @@
+// we're going to do most of the work in here.
+
+// TODO: find the HTML container for posts in the group by ID
+
+// TODO: take in input through a floating dialog box
+
+// TODO: filter by name, hopefully there's a common class 
+
+// TODO: remove parent container for each posts
