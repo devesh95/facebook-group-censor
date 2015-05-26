96fa45c2a8e68952747e0f97fe68405032f6c9c0
Devesh Dayal <devesh95@gmail.com>
2015-05-27 00:43:10 +0530

    integrated into facebook

--- content_scripts/main.js
+++ content_scripts/main.js
@@ -1,4 +1,51 @@
-var CENSOR_TAG = '<p class="sensor"> <span style="background-color: #B33A3A; color: #fff">&nbsp;Warning!&nbsp;</span>&nbsp;This post has been censored.<a style="float:right" class="reset _42ft _4jy0 _4jy3 _517h _51sy" role="button">Show Anyway</a></p>';
+var CENSOR_TAG = '<p class="censor"> <span style="background-color: #B33A3A; color: #fff">&nbsp;Warning!&nbsp;</span>&nbsp;This post has been censored.<a style="float:right" class="reset _42ft _4jy0 _4jy3 _517h _51sy" role="button">Show Anyway</a></p>';
+
+var inputBox = "";
+inputBox += "<div class=\"form-style-6\" style=\"width: inherit";
+inputBox += "    height: 250px; margin-left: 15px; margin-top: 15px;\">";
+inputBox += "      <h1 style=\"background: #3B5998;";
+inputBox += "    padding: 20px 0;";
+inputBox += "    font-size: 140%;";
+inputBox += "    font-weight: 300;";
+inputBox += "    text-align: center;";
+inputBox += "    color: #fff;";
+inputBox += "    margin: -16px -16px 16px -16px;\">Facebook Group Censor<\/h1>";
+inputBox += "      <form style=\"font: 95% Arial, Helvetica, sans-serif;";
+inputBox += "    max-width: 95%;";
+inputBox += "    margin: 10px auto;";
+inputBox += "    padding: 16px;";
+inputBox += "    background: #F7F7F7;";
+inputBox += "\">";
+inputBox += "        <p style=\"font-size: 14px;";
+inputBox += "    margin: 10px;\">Enter names below (separated by commas) to be censored.<\/p>";
+inputBox += "        <input style=\"-webkit-transition: all 0.30s ease-in-out;";
+inputBox += "    -moz-transition: all 0.30s ease-in-out;";
+inputBox += "    -ms-transition: all 0.30s ease-in-out;";
+inputBox += "    -o-transition: all 0.30s ease-in-out;";
+inputBox += "    outline: none;";
+inputBox += "    box-sizing: border-box;";
+inputBox += "    -webkit-box-sizing: border-box;";
+inputBox += "    -moz-box-sizing: border-box;";
+inputBox += "    width: 100%;";
+inputBox += "    background: #fff;";
+inputBox += "    margin-bottom: 4%;";
+inputBox += "    border: 1px solid #ccc;";
+inputBox += "    padding: 3%;";
+inputBox += "    color: #555;";
+inputBox += "    font: 95% Arial, Helvetica, sans-serif;\" id=\"names\" type=\"text\" name=\"names\" placeholder=\"Devesh, Jashan, etc.\" \/>";
+inputBox += "        <input style=\"box-sizing: border-box;";
+inputBox += "    -webkit-box-sizing: border-box;";
+inputBox += "    -moz-box-sizing: border-box;";
+inputBox += "    width: 100%;";
+inputBox += "    padding: 3%;";
+inputBox += "    background: #3B5998;";
+inputBox += "    border-bottom: 2px solid #0e1f5b;";
+inputBox += "    border-top-style: none;";
+inputBox += "    border-right-style: none;";
+inputBox += "    border-left-style: none;    ";
+inputBox += "    color: #fff;\" id=\"censor-on\" type=\"button\" value=\"Start Censoring\" \/>";
+inputBox += "      <\/form>";
+inputBox += "    <\/div>";
 
 var findPosts = function (name) {
 	var posts = [];
@@ -72,17 +119,21 @@ var censorPostContent = function (posts) {
     console.log(window.content_list);
 };
 
-window.onload = function() {
-	console.log('Running script on a Facebook group page.');
-	// run using local storage data
-	findPosts('');
+window.init = function (input) {
+	if (!input) {
+		return;
+	}
+	var names = input.split(',');
+	for (var i = 0; i < names.length; i++) {
+		findPosts(names[i]);
+	}
 
 	$('.reset').on('click', function () {
 		var post = $(this);
 		var elem = $(post).parents('.userContentWrapper');
 		var id  = elem.data('gt').fbstory;
 		elem.children().last().remove();
-		elem.find('.sensor').each(function (index) {
+		elem.find('.censor').each(function (index) {
 			console.log($(this).text());
 			$(this).replaceWith($(window.content_list[id].originalContent))
 		});
@@ -93,5 +144,23 @@ window.onload = function() {
 	});
 };
 
+window.onload = function () {
+	console.log('Running script on a Facebook group page.');
 
-// TODO: take in input through a floating dialog box
+	$('#pagelet_group_').prepend(inputBox);
+
+	var names = '';
+
+    $('#censor-on').on('click', function () {
+        window.init(names);
+    });
+
+    $("#names").on('keyup', function (e) {
+        var text = $('#names').val();
+		names = text;
+		var code = (e.keyCode ? e.keyCode : e.which);
+		if (code == 13) {
+			window.init(names);
+		}
+	});
+};
